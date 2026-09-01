0b24879fe635c2304a47108beaf79e4a3c704974
Tazz022003 <almoceracharsgerand@gmail.com>
2026-09-02 06:43:39 +0800

    add crud

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -21,9 +21,14 @@ from studentorg import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('', views.HomePageView.as_view(), name='home'),
+
     path('organization_list/', OrganizationList.as_view(), name='organization-list'),
+
     path('organization_list/add/', OrganizationCreateView.as_view(), name='organization-add'),
+
     path('organization_list/<int:pk>/', OrganizationUpdateView.as_view(), name='organization-update'),
-    path('organization_list/<pk>/delete', OrganizationDeleteView.as_view(), name='organization-delete'),
+
+    path('organization_list/<int:pk>/delete/', OrganizationDeleteView.as_view(), name='organization-delete'),
 ]
